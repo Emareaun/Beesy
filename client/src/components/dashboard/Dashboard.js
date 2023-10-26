b11834c5c319b23748f50c4635cc8bc0f1cb8fad
emareaun <emareaun12345@gmail.com>
2023-10-26 19:28:10 -0400

    dash2

--- client/src/components/dashboard/Dashboard.js
+++ client/src/components/dashboard/Dashboard.js
@@ -1,1 +1,13 @@
 import React, { useState } from 'react';
+const Dashboard = () => {
+  const [title, setTitle] = useState('');
+  const [description, setDescription] = useState('');
+  const [image, setImage] = useState(null);
+
+  const handleTitleChange = (e) => {
+    setTitle(e.target.value);
+  };
+
+  const handleDescriptionChange = (e) => {
+    setDescription(e.target.value);
+  };
