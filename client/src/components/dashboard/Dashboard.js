b4df8471897e34dc26d1b77a320cb194e180e6e6
emareaun <emareaun12345@gmail.com>
2023-10-26 19:37:25 -0400

    dash edit

--- client/src/components/dashboard/Dashboard.js
+++ client/src/components/dashboard/Dashboard.js
@@ -1,4 +1,5 @@
 import React, { useState } from 'react';
+
 const Dashboard = () => {
   const [title, setTitle] = useState('');
   const [description, setDescription] = useState('');
@@ -11,3 +12,47 @@ const Dashboard = () => {
   const handleDescriptionChange = (e) => {
     setDescription(e.target.value);
   };
+
+  const handleImageChange = (e) => {
+    const file = e.target.files[0];
+    setImage(file);
+  };
+
+  const handleSubmit = () => {
+    // Handle blog creation and submission
+    // send data backend 
+    const blogData = {
+      title,
+      description,
+      image,
+    };
+
+  };
+
+  return (
+    <div className="dashboard">
+      <h2>Dashboard</h2>
+      <div className="blog-form">
+        <input
+          type="text"
+          placeholder="Blog Title"
+          value={title}
+          onChange={handleTitleChange}
+        />
+        <textarea
+          placeholder="Blog Description"
+          value={description}
+          onChange={handleDescriptionChange}
+        />
+        <input
+          type="file"
+          accept="image/*"
+          onChange={handleImageChange}
+        />
+        <button onClick={handleSubmit}>Publish Blog</button>
+      </div>
+    </div>
+  );
+};
+
+export default Dashboard;
