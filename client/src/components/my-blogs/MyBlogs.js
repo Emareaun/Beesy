7f37f7d25f2d832f9416988bc1395c19dbd914c0
emareaun <emareaun12345@gmail.com>
2023-10-26 20:32:50 -0400

    r and m

--- client/src/components/my-blogs/MyBlogs.js
+++ client/src/components/my-blogs/MyBlogs.js
@@ -7,3 +7,24 @@ const MyBlogs = () => {
   useEffect(() => {
    
   }, [userId]);
+  return (
+    <div className="my-blogs">
+      <h2>My Blogs</h2>
+      {userBlogs.length === 0 ? (
+        <p>No blogs found.</p>
+      ) : (
+        <ul>
+          {userBlogs.map((blog) => (
+            <li key={blog.id}>
+              <h3>{blog.title}</h3>
+              <p>{blog.description}</p>
+              <img src={blog.imageUrl} alt="Blog" />
+            </li>
+          ))}
+        </ul>
+      )}
+    </div>
+  );
+};
+
+export default MyBlogs;
