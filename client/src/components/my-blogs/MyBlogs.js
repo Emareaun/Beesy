60e2e20a646d3f913aeec9150eb23bd1c8af798d
emareaun <emareaun12345@gmail.com>
2023-10-26 20:07:39 -0400

    admin page

---
+++ client/src/components/my-blogs/MyBlogs.js
@@ -0,0 +1,9 @@
+import React, { useState, useEffect } from 'react';
+
+const MyBlogs = () => {
+  const [userBlogs, setUserBlogs] = useState([]);
+  const userId = 123; 
+
+  useEffect(() => {
+   
+  }, [userId]);
