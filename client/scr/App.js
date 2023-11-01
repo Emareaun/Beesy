7a3059e1c2576c5f70e01ecd0e2b2b4d0c9da5e9
emareaun <emareaun12345@gmail.com>
2023-10-31 22:32:45 -0400

    update

--- client/scr/App.js
+++ client/scr/App.js
@@ -1,16 +1,16 @@
 import React from 'react';
 import { BrowserRouter as Router, Route, Switch } from 'react-router-dom';
-import LandingPage from '../components/LandingPage';
-import Login from '../components/Login'; // Create a Login component
-import Signup from '../components/Signup'; // Create a Signup component
+import LandingPage from './components/LandingPage'; 
+import Login from './components/Login'; 
+import Signup from './components/Signup'; 
 
 function App() {
   return (
     <Router>
       <Switch>
         <Route exact path="/" component={LandingPage} />
-        <Route path="/login" component={Login} />
-        <Route path="/signup" component={Signup} />
+        <Route path="/login" component={Login} /> {/* Login route */}
+        <Route path="/signup" component={Signup} /> {/* Signup route */}
       </Switch>
     </Router>
   );
