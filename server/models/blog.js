7bbedd05aca6302147d0d0e3242da2ebd63439cf
emareaun <emareaun12345@gmail.com>
2023-11-02 21:18:50 -0400

    upadte

--- server/models/blog.js
+++
@@ -1,9 +0,0 @@
-const mongoose = require('mongoose');
-
-const blogSchema = new mongoose.Schema({
-  title: String,
-  description: String,
-  imageUrl: String,
-});
-
-module.exports = mongoose.model('Blog', blogSchema);
